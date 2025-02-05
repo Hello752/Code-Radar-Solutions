6d26a0edadbf99690c3443ede5db274994430684
Saideep ghosh <ghoshsaideep@gmail.com>
2025-02-05 17:28:37 +0530

    Solution to Perform Bitwise OR

--- Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -4,7 +4,7 @@ int main() {
     int num1, num2, result;
 
     // Input two integers
-    printf("Enter the first integer: ");
+
     scanf("%d", &num1);
     printf("Enter the second integer: ");
     scanf("%d", &num2);
@@ -13,7 +13,7 @@ int main() {
     result = num1 | num2;
 
     // Print the result
-    printf("Result of %d | %d = %d\n", num1, num2, result);
+    printf(" %d = %d\n", num1, num2, result);
 
     return 0;
 }
