2aa08ac0bc61ecb17ea706ef88ec44ea730f4198
Saideep ghosh <ghoshsaideep@gmail.com>
2025-02-06 17:06:43 +0530

    Solution to Check Alphabet Case

--- Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int main() 
-{
+int main() {
     char ch;
-    
-    // Prompt the user to enter a year
+  
     scanf("%c", &ch);
-    
-    // Check if the year is a leap year
-    if(isupper(ch)){
-        printf("Uppercase",ch);
-    }
-    else if (islower(ch)){
+
+    if (isupper(ch)) {
+        printf("Uppercase ",ch);
+    } else if (islower(ch)) {
         printf("Lowercase",ch);
-    }
-    else{
-        printf("Not an alphabet",ch);
+    } else {
+        printf("Not an Alphabet",ch);
     }
     return 0;
-    
-
 }
