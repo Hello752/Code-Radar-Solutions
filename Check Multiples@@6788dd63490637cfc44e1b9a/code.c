3598e65a1ca3a134509d2a1f89b2fe229efda2e4
Saideep ghosh <ghoshsaideep@gmail.com>
2025-02-11 18:59:28 +0530

    Solution to Check Multiples

--- Check Multiples@@6788dd63490637cfc44e1b9a/code.c
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -3,7 +3,7 @@ int main (){
     int number,anumber;
     scanf("%d %d ",&number,&anumber);
     if(number%anumber==0){printf("Yes");}
-    else(printf("No");)
+    else{printf("No");}
     return 0;
 
 }
