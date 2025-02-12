05e588a655484b72868802dd1596270effdbff50
Saideep ghosh <ghoshsaideep@gmail.com>
2025-02-12 16:35:23 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,a,operator;
+    scanf("%d %d",&n,&a,&operator);
+    if (operator==+){
+    printf("n"operator"a");}
+    elseif(operator==-){
+        printf("n"operator"a")
+    }
+    return 0;
+
+}
