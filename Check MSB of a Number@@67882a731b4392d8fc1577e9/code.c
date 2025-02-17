9d4015dc58a9d206c8aac5ce0470665de465cda6
Saideep ghosh <ghoshsaideep@gmail.com>
2025-02-17 22:39:32 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -7,7 +7,7 @@ int main() {
     // Calculate the MSB mask for 32-bit integer
     int msb_mask = 1 << 31;
 
-    if (num & msb_mask==1) {
+    if (num & msb_mask>1) {
         printf("Set");
     } else {
         printf("Not Set");
