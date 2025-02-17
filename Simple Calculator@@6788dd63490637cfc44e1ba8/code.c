a8ccab2b1b88f0afa682bfc57e6202e8a48f1563
Saideep ghosh <ghoshsaideep@gmail.com>
2025-02-17 20:01:10 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,26 +1,24 @@
 #include<stdio.h>
 int main(){
-    int a,n;
-    char op;
-    scanf("%d %d %c",&a,&n,&op);
-   if(op=='+'){
-    int sum;
-    sum=a + n;
-    printf("%d",sum );}
+    int a,n,op;
+    scanf("%d %d %d",&a,&n,&op);
+    if (op=='+'){
+        int sum;
+        sum=a + n;
+        printf("%d",sum);}
+    else if(op=='-'){int sub;
+    sub=a-n;
+    printf("%d",&sub);}
     else if(op=='-'){
-        int sub;
-        sub= a-n;
-        printf("%d",sub);}
-     else if(op=='*'){
         int mul;
-        mul= a*n;
-        printf("%d",mul);}
-     else if(op=='/'){
-        if(n==0){printf("error");}
-         else{  int div;
-            div= a/n;
-        printf("%d",div);}}
+        mul=a*n;
+        printf("%d",mul);
+    }
+    else if(op=='/'){
+        if(n==0){printf("Error");}
+        else{int div;
+        div =a/n;
+        printf("&d",div);}
+    }
     return 0;
-
-
-}
+}  
