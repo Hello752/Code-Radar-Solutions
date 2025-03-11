fbddfd65d829e943a89eec15dd8b31b985c647ef
Saideep ghosh <ghoshsaideep@gmail.com>
2025-03-11 20:03:27 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>  // For malloc and free
 
+// Function to perform Bubble Sort
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -14,8 +15,9 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
+// Function to print array elements
 void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {  // Fixed loop
+    for (int i = 0; i < n; i++) {  // FIXED LOOP CONDITION
         printf("%d ", arr[i]);
     }
     printf("\n");
@@ -23,7 +25,15 @@ void printArray(int arr[], int n) {
 
 int main() {
     int n;
+    
+    // Ask user for input
+    printf("Enter number of elements: ");
     scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Invalid array size\n");
+        return 1; // Exit with error
+    }
 
     // Allocate memory dynamically
     int *arr = (int *)malloc(n * sizeof(int));
@@ -32,11 +42,15 @@ int main() {
         return 1;
     }
 
+    // Read array elements
+    printf("Enter %d elements: ", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
+    // Sort and print array
     bubbleSort(arr, n);
+    printf("Sorted array: ");
     printArray(arr, n);
 
     // Free allocated memory
