0b70529f3279e2fade957b7bdd488631392e5afd
Saideep ghosh <ghoshsaideep@gmail.com>
2025-04-08 16:23:16 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -3,9 +3,9 @@
 
 // Function to check if a number is prime
 int isPrime(int number) {
-    if (number < 2) return 0;         // 0 and 1 are not prime
-    if (number == 2) return 1;        // 2 is prime
-    if (number % 2 == 0) return 0;    // Even numbers > 2 are not prime
+    if (number ==0 || number==1) return 0;         // 0 and 1 are not prime
+            // 2 is prime
+    // if (number % 2 == 0) return 0;    // Even numbers > 2 are not prime
 
     // Check for factors from 3 up to sqrt(number)
     for (int i = 3; i*i<=number ; i ++) {
